f90e9973422890eaae75f8c3ac1966097d08b1b5
Dinne Kopelevich <dinne22@hotmail.com>
2023-01-27 17:54:08 -0700

    Add book.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,43 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return { name, age, pronouns };
+}
+
+function saveReview(bookReviews, reviews) {
+  if (reviews.includes(bookReviews)) {
+    return reviews.length;
+  } else {
+    reviews.push(bookReviews);
+  }
+}
+
+function calculatePageCount(bookTitle, bookPageCount) {
+  return (bookPageCount = bookTitle.length * 20);
+}
+
+function writeBook(title, mainCharacter, genre) {
+  var book = {
+    title,
+    mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre,
+  };
+  return book;
+}
+
+function editBook(ghoulBook) {
+  ghoulBook.pageCount = ghoulBook.pageCount * 0.75;
+  return ghoulBook;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
-}
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook,
+};
