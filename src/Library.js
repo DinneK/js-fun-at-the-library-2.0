3656fc078357959a039a8467b5af3496c869f1b8
Dinne Kopelevich <dinne22@hotmail.com>
2023-02-06 12:07:03 -0700

    Add Library.js

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,43 @@
+function createLibrary(libraryInfo) {
+  const library = {
+    name: libraryInfo,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    },
+  };
+  return library;
+}
 
+function addBook(shelf, book) {
+  if (book.genre === "fantasy") {
+    shelf.shelves.fantasy.push(book);
+  }
+  if (book.genre === "nonFiction") {
+    shelf.shelves.nonFiction.push(book);
+  }
+  if (book.genre === "fiction") {
+    shelf.shelves.fiction.push(book);
+  }
+}
+
+function checkoutBook(library, title, genre) {
+  const unavailable = `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+  const available = `You have now checked out ${title} from the ${library.name}`;
+
+  const bookIndex = library.shelves[genre].findIndex(
+    (book) => book.title === title
+  );
+  if (bookIndex === -1) {
+    return unavailable;
+  }
+  library.shelves[genre].splice(bookIndex, 1);
+  return available;
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook,
 };
