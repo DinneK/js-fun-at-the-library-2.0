0f8ed970af0b68c636ed29a3dde31c7cfbc85968
Dinne Kopelevich <dinne22@hotmail.com>
2023-02-07 09:47:14 -0700

    Add librarian

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,31 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+
+  greetPatron(name, condition) {
+    if (condition) {
+      return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+
+  findBook(book) {
+    const foundBook = this.library.shelves.fantasy.find((title) => title.title);
+    if (book === foundBook.title) {
+      this.library.shelves.fantasy.pop();
+      return `Yes, we have ${book}`;
+    } else {
+      return `Sorry, we do not have ${book}`;
+    }
+  }
+
+  calculateLateFee(days) {
+    const lateFee = days * 0.25;
+    const roundedFee = Math.ceil(lateFee);
+    return roundedFee;
   }
 }
 
